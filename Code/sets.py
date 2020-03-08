43e9d71d90dce5d3fa06fd00c3e174bfcfdbe5af
alexbarksdale <ale.barksdale@gmail.com>
2020-03-08 18:01:06 -0500

    Time complexity

--- Code/sets.py
+++ Code/sets.py
@@ -10,25 +10,34 @@ class Set(object):
                 self.add(el)
 
     def size(self):
+        "O(1) - HT keeps track of it's size"
         return self.ht.size
 
     def contains(self, el):
+        """Best case running time: O(1) This hash table is using a small load factor 
+        Worst case running time: O(l) - l being the load factor"""
         return self.ht.contains(el)
 
     def add(self, el):
+        """Best case running time: O(1) This hash table is using a small load factor 
+        Worst case running time: O(l) - l being the load factor"""
         if not self.contains(el):
             self.ht.set(el, el)
 
     def remove(self, el):
+        """Best case running time: O(1) This hash table is using a small load factor 
+        Worst case running time: O(l) - l being the load factor"""
         # Does contain check is handled in HashTable
         self.ht.delete(el)
 
     def union(self, other_set):
+        """O(m + n)"""
         return Set(self.ht.keys() + other_set.ht.keys())
 
     def intersection(self, other_set):
-        # TODO: Time complexity and add a check to prevent having to enter the loop. Something similar to is_subset
-
+        # TODO: add a check to prevent having to enter the loop. Something similar to is_subset
+        # O(min(m, n)) because we iterate over the smaller set
+        """O(n) because the hash table's key() takes linear time"""
         n_set = Set()
         for el in self.ht.keys():
             if other_set.contains(el):
@@ -36,7 +45,8 @@ class Set(object):
         return n_set
 
     def difference(self, other_set):
-        # TODO: Time complexity and add a check to prevent having to enter the loop. Something similar to is_subset
+        # TODO: add a check to prevent having to enter the loop. Something similar to is_subset
+        """O(n) because the hash table's key() takes linear time"""
         n_set = Set()
         for el in self.ht.keys():
             if not other_set.contains(el):
@@ -44,6 +54,8 @@ class Set(object):
         return n_set
 
     def is_subset(self, other_set):
+        """O(n) because the hash table's key() takes linear time"""
+        n_set = Set()
         if other_set.size > self.ht.size:
             return False
 
